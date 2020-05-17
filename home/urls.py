b39a8cc81cb102176551224599cbf7e6c4c52fe3
melekzurnaci <mlkzrnc@gmail.com>
2020-05-17 05:35:24 +0300

    havalinı Transfer sitesi hakkımızda , referans , iletişim sayfaları oluşturuldu

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # ex: /home/
     path('', views.index, name='index'),
+
     # ex: /polls/5/
     #path('<int:question_id>/', views.detail, name='detail'),
 
