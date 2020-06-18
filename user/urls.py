f0576cc8db89b923741182fe2dcd4f7b34c98ff3
melekzurnaci <37675508+melekzurnaci@users.noreply.github.com>
2020-06-18 13:30:18 +0300

    django projem user delete işlemleri

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('update/', views.user_update, name='user_update'),
     path('password/', views.change_password, name='change_password'),
-  #  path('addcomment/<int:id>', views.addcomment, name='addcomment'),
+    path('transfers/', views.transfers, name='transfers'),
+    path('transferdetail/<int:id>', views.transferdetail, name='transferdetail'),
+    path('comments/', views.comments, name='comments'),
+path('deletecomment/<int:id>', views.deletecomment , name='deletecomment'),
+    #  path('addcomment/<int:id>', views.addcomment, name='addcomment'),
 
     # ex: /polls/5/
     #path('<int:question_id>/', views.detail, name='detail'),
